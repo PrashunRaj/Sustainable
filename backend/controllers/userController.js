32837138be4054537e7565004608e3ce0b68ccf6
PrashunRaj <137059773+PrashunRaj@users.noreply.github.com>
2025-02-04 13:33:11 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,41 +77,41 @@ const registerUser = async (req, res) =>{
 //     try{
 //         const {name,email,password}=req.body
 
-        if(!name || !password || !email){
-            return res.json({success:false,message:"Missing Details"})
+//         if(!name || !password || !email){
+//             return res.json({success:false,message:"Missing Details"})
 
-        }
-        if(!validator.isEmail(email)){
-            return res.json({success:false,message:"enter a valid email"})
+//         }
+//         if(!validator.isEmail(email)){
+//             return res.json({success:false,message:"enter a valid email"})
 
-        }
-        if(password.length<8){
-            return res.json({success:false,message:"enter a strong password"})
-        }
+//         }
+//         if(password.length<8){
+//             return res.json({success:false,message:"enter a strong password"})
+//         }
 
-        //hashing user password
-        const salt=await bcrypt.genSalt(10)
-        const hashedPassword=await bcrypt.hash(password,salt)
+//         //hashing user password
+//         const salt=await bcrypt.genSalt(10)
+//         const hashedPassword=await bcrypt.hash(password,salt)
 
-        const userData={
-            name,
-            email,
-            password:hashedPassword
-        }
-        const newUser=new userModel(userData)
-        const user=await newUser.save()
+//         const userData={
+//             name,
+//             email,
+//             password:hashedPassword
+//         }
+//         const newUser=new userModel(userData)
+//         const user=await newUser.save()
 
-        const token=jwt.sign({id:user._id},process.env.JWT_SECRET)
-        res.json({success:true,token})
-        const token=jwt.sign({id:user._id},process.env.JWT_SECRET)
-        res.json({success:true,token})
+//         const token=jwt.sign({id:user._id},process.env.JWT_SECRET)
+//         res.json({success:true,token})
+//         const token=jwt.sign({id:user._id},process.env.JWT_SECRET)
+//         res.json({success:true,token})
 
 
-    }catch(error){
-        console.log(error)
-        res.json({success:false,message:error.message})
-    }
-}
+//     }catch(error){
+//         console.log(error)
+//         res.json({success:false,message:error.message})
+//     }
+// }
 //Api for user Login
 const loginUser=async(req,res)=>{
     try {
